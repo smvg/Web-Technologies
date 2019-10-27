20f309236e84f8e43cf5cb757bc96676b3d49c5e
Sam Vargas <samfefuty@hotmail.com>
2019-10-28 00:34:56 +0100

    First version of admin website. Ugly for now. Have to work on it.

---
+++ shared/js/admin.js
@@ -0,0 +1,43 @@
+var content_ids = ['dashboard', 'accounts', 'rooms', 'location'];
+
+function hide(value) {
+    var content = document.getElementById(value);
+
+    content.style.visibility = 'hidden';
+};
+
+function make_content_visible(id) {
+
+    console.log("Entro en la funcion");
+
+    var content = document.getElementById(id);
+
+    content_ids.forEach(hide);
+
+    content.style.visibility = 'visible';
+    
+};
+
+
+function swap_on_save(id) {
+    var email = document.getElementById('email-' + id);
+    var password = document.getElementById('password-' + id);
+    var button = document.getElementById('button-' + id);
+
+    email.innerHTML = "mark@email.com"
+    password.innerHTML = "********"
+    button.innerHTML = "<a href='#' class='btn btn-secondary' onclick='swap_on_edit(" + id + ")'>Edit</a>"
+
+    alert('Account has been succesfully updated!');
+};
+
+function swap_on_edit(id) {
+    
+    var email = document.getElementById('email-' + id);
+    var password = document.getElementById('password-' + id);
+    var button = document.getElementById('button-' + id);
+
+    email.innerHTML = "<input type='email'>"
+    password.innerHTML = "<input type='password'>"
+    button.innerHTML = "<a href='#' class='btn btn-success' onclick='swap_on_save(" + id + ")'>Save</a>"
+};
